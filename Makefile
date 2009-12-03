bba65bb169714ed255d16d80cb67f6ff7608d402
Spencer Russell <spencer.f.russell@gmail.com>
2009-12-03 11:44:43 -0500

    dimmer fully working, just breathes.

--- Makefile
+++ Makefile
@@ -6,16 +6,18 @@ CC=/usr/bin/avr-gcc
 CFLAGS=-g -Os -Wall -Werror -mcall-prologues -mmcu=$(MMCU) -lm
 OBJ2HEX=/usr/bin/avr-objcopy 
 AVRPROG=/usr/local/bin/avrdude
-AVROPTS=-B4 -Pusb
+AVROPTS=-B5 -Pusb
 
-program : $(TARGET).hex
-	$(AVRPROG) -c$(PROGRAMMER) -p$(MMCU) $(AVROPTS) -Uflash:w:$(TARGET).hex
+$(TARGET).hex:
+
+%.hex : %.obj
+	$(OBJ2HEX) -R .eeprom -O ihex $< $@
 
 %.obj : %.o
 	$(CC) $(CFLAGS) $< -o $@
 
-%.hex : %.obj
-	$(OBJ2HEX) -R .eeprom -O ihex $< $@
+burn: $(TARGET).hex
+	$(AVRPROG) -c$(PROGRAMMER) -p$(MMCU) $(AVROPTS) -Uflash:w:$(TARGET).hex
 
 clean :
 	rm -f *.hex *.obj *.o
--- proxlamp.c
+++ proxlamp.c
@@ -9,14 +9,22 @@
 #define TRIAC_PIN (1 << PD0)
 
 #define SENSOR_PORT PORTB
+#define SENSOR_DD DDRB
+#define SENSOR_PINS PINB
 
 #define ZEROCROSS_PORT PORTD
+#define ZEROCROSS_DD DDRD
+#define ZEROCROSS_PINS PIND
 #define ZEROCROSS_PIN (1 << PD2)
+
+#define TEST_PORT PORTD
+#define TEST_DD DDRD
+#define TEST_PIN (1 << PD1)
 
 /* Global Variables */
 
-/* measure the half-cycle duration in cycles */
-uint32_t ac_halfcycle = F_CPU / 120;
+/* cycles to hold the triac high for triggering */
+const uint16_t TRIGGER_TIME = 1E-4 * F_CPU;
 volatile uint32_t pulse_width;
 
 
@@ -30,59 +38,111 @@ void clock_setup(void) {
 	/* set up 16-bit counter with prescalar of 8 */
 	TCCR1A = 0x00;
 	TCCR1B = 0x02;
-	/* enable overflow interrupt for timer1 */
-	TIMSK1 = 0x01;
 
-	/* enable interrupts */
-	sei();
 }
 
 void pin_io_setup(void) {
-	TRIAC_DD |= TRIAC_PIN; /* set the triac pin to output */
-	TRIAC_PORT |= TRIAC_PIN;
+	/* set the triac pin to output */
+	TRIAC_DD |= TRIAC_PIN; 
+	/* start in low state */
+	TRIAC_PORT &= ~TRIAC_PIN; 
+
+	/* set the test pin to output */
+	TEST_DD |= TEST_PIN; 
+	/* start in low state */
+	TEST_PORT &= ~TEST_PIN; 
+
+	/* set the zero cross detector pin to input */
+	ZEROCROSS_DD &= ~ZEROCROSS_PIN; 
+	/* disable pullup resistor on zero-cross detector */
+	ZEROCROSS_PORT &= ~ZEROCROSS_PIN;
+
+	/* set the sensor port to all input */
+	SENSOR_DD = 0x00;
+	/* enable pull-up resistors on sensor input */
+	SENSOR_PORT = 0xFF;
+
+	/* set up INT0 interrupt pin to trigger on both edges */
+	EICRA = 0x01;
+	/* enable interrupt from INT0 */
+	EIMSK = 0x01;
 }
 
 /* schedule a timer interrupt in cycles */
 inline void set_alarm1(uint32_t cycles) {
-	TCNT1 = UINT16_MAX - (cycles / 8) + TCNT1;
+	TCNT1 = UINT16_MAX - (cycles / 8);
+}
+
+/* INT0 zero-crossing interrupt handler */
+
+ISR(INT0_vect) {
+	/* sample 5 more times to check for noise */
+	char val = ZEROCROSS_PINS & ZEROCROSS_PIN;
+	int n = 50;
+	while(n--)
+		if((ZEROCROSS_PINS & ZEROCROSS_PIN) != val)
+			return;
+	TEST_PORT ^= TEST_PIN;
+	TRIAC_PORT &= ~TRIAC_PIN;
+	set_alarm1(pulse_width);
+
+	/* clear any pending timer interrupts */
+	TIFR1 |= 1 << TOV1;
+
+	/* enable overflow interrupt for timer1 */
+	TIMSK1 = 0x01;
+
+	/* disable interrupt from INT0 so we don't re-trigger */
+	EIMSK = 0x00;
 }
 
 /* timer1 overflow interrupt handler */
 ISR(TIMER1_OVF_vect) {
 	if(TRIAC_PORT & TRIAC_PIN) {
 		TRIAC_PORT &= ~TRIAC_PIN;
-		set_alarm1(ac_halfcycle - pulse_width);
+		/* disable overflow interrupt for timer1 */
+		TIMSK1 = 0x00;
+
+		/* clear any pending external interrupts */
+		EIFR |= 1 << INTF0;
+		/* enable interrupt from INT0, wait for next zero-crossing */
+		EIMSK = 0x01;
 	}
 	else {
 		TRIAC_PORT |= TRIAC_PIN;
-		set_alarm1(pulse_width);
+		set_alarm1(TRIGGER_TIME);
 	}
 }
 
 int main(void) {
+	float brightness = 0;
+
+	/* measure the half-cycle duration in cycles */
+	/* 8.3ms in my apartment @ 8MHz = 66400 cycles */
+	uint32_t ac_halfcycle = 66400;
+
+	int brightness_period = 10000;
+	int elapsed_ms = 0;
+
 	clock_setup();
 	pin_io_setup();
 	
-	float brightness = 0;
-	int brightness_period = 1000;
-	int elapsed_ms = 0;
-
-	/* kickstart timer */
-	TCNT1 = UINT16_MAX - 1;
+	/* enable interrupts */
+	sei();
 
 	while(1) {
 		if(elapsed_ms < brightness_period) {
 			brightness = (float)elapsed_ms / brightness_period;
-			if(brightness > 0.99)
-				brightness = 0.99;
-			pulse_width = brightness * ac_halfcycle;
+			pulse_width = 0.95 * brightness * ac_halfcycle;
+			//if(pulse_width > ac_halfcycle - TRIGGER_TIME - 100)
+			//	pulse_width = ac_halfcycle - TRIGGER_TIME - 100;
 		}
 		else if(elapsed_ms < 2 * brightness_period) {
 			brightness = 1 - (float)(elapsed_ms - brightness_period) / 
 				brightness_period;
-			if(brightness > 0.99)
-				brightness = 0.99;
-			pulse_width = brightness * ac_halfcycle;
+			pulse_width = 0.95 * brightness * ac_halfcycle;
+			//if(pulse_width > ac_halfcycle - TRIGGER_TIME - 100)
+				//pulse_width = ac_halfcycle - TRIGGER_TIME - 100;
 		}
 		else 
 			elapsed_ms = 0;
